a8579b197c2ad98b27baba622ed2f1f042939e9f
ItsTulasiRaman <tulasiraman917@gmail.com>
2023-07-13 19:56:48 +0530

    Full configuration

--- app/src/main/java/com/scm/fopups/MainActivity.java
+++ app/src/main/java/com/scm/fopups/MainActivity.java
@@ -1,17 +1,27 @@
 package com.scm.fopups;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -21,9 +31,20 @@ import com.scm.fopups.API_Calls.RetrofitAPI;
 import com.scm.fopups.databinding.ActivityMainBinding;
 import com.scm.fopups.ui.todo.ToDoFragment;
 
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -33,17 +54,21 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
     TrackedAppHelper dbHelper;
     ToDoHandler tdb;
 
+    private final String FLASK_ENDPOINT = "http://172.23.20.2:5000/askgpt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -55,6 +80,18 @@ public class MainActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Fopups-Channel";
+            String description = "App Alarm";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("Fopup-Notification", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         tdb = new ToDoHandler(getApplicationContext());
         tdb.openDatabase();
         List<ToDo> taskList = tdb.getAllTasks();
@@ -63,17 +100,96 @@ public class MainActivity extends AppCompatActivity{
         for (ToDo task : taskList) {
             taskStrings.add(task.getTask());
         }
+
+        Log.d("Api Call", post_data(taskStrings.toString()));
         Timer timer = new Timer();
         TimerTask hourlyTask = new TimerTask() {
             @Override
             public void run() {
-                Log.d("Timer tasks: ", taskStrings.toString());
+                //post_data(taskStrings.toString());
+                Log.d("Notify", taskStrings.toString());
+                int max = taskStrings.size();
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "Fopup-Notification");
+                builder.setContentTitle("Limit Exceeded");
+                int b = (int)(Math.random()*(max)+0);
+                builder.setContentText(taskStrings.get(b));
+                builder.setSmallIcon(R.drawable.warning);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                managerCompat.notify(1, builder.build());
+
             }
         };
-        timer.schedule(hourlyTask, 0l, 6000);
+        timer.schedule(hourlyTask, 0l, 90000);
+
+
 
     };
 
+
+
+    public String post_data(String input){
+        String gen_message = "";
+
+        if (input!=null) {
+            String query = input.toString();
+            String prompt = query;
+
+            try
+            {
+                URL url = new URL(FLASK_ENDPOINT);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                // Construct the request body
+                String requestBody = "Message=" + prompt;
+
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(requestBody.getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    // Extract the generated message from the JSON response
+                    gen_message = response.substring(12, (response.length() - 2));
+                    gen_message = StringEscapeUtils.unescapeJava(gen_message);
+                } else {
+                    gen_message = "API request failed with response code: " + responseCode;
+                }
+
+                connection.disconnect();
+            } catch(Exception e)
+            {
+                e.printStackTrace();
+                gen_message = "API request failed: " + e.toString();
+
+            }
+        }
+        return gen_message;
+    }
+
+/*
     private void postData(String tasks) {
 
         // on below line we are creating a retrofit
@@ -115,7 +231,7 @@ public class MainActivity extends AppCompatActivity{
             }
         });
     }
-
+*/
 
     @Override
     protected void onResume() {
--- app/src/main/java/com/scm/fopups/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/scm/fopups/ui/notifications/NotificationsFragment.java
@@ -1,20 +1,44 @@
 package com.scm.fopups.ui.notifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.scm.fopups.MainActivity;
+import com.scm.fopups.R;
+import com.scm.fopups.ToDo;
+import com.scm.fopups.ToDoHandler;
 import com.scm.fopups.databinding.FragmentNotificationsBinding;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    Button send;
+    TextView responseView;
+    ToDoHandler tdb;
+    private final String FLASK_ENDPOINT = "http://172.23.20.2:5000/askgpt";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +48,107 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        send = root.findViewById(R.id.sendButton);
+        responseView = root.findViewById(R.id.response);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Fopups-Channel";
+            String description = "App Alarm";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("Fopup-Notification", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system. You can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+
+        tdb = new ToDoHandler(getContext());
+        tdb.openDatabase();
+        List<ToDo> taskList = tdb.getAllTasks();
+        tdb.close();
+        List<String> taskStrings = new ArrayList<>();
+        for (ToDo task : taskList) {
+            taskStrings.add(task.getTask());
+        }
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String response = post_data(taskStrings.toString());
+                int max = taskStrings.size();
+                int b = (int)(Math.random()*(max)+0);
+                responseView.setText(taskStrings.get(b));
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), "Fopup-Notification");
+                builder.setContentTitle("Limit Exceeded");
+                builder.setContentText(taskStrings.get(0));
+                builder.setSmallIcon(R.drawable.warning);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getContext());
+                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                managerCompat.notify(1, builder.build());
+
+            }
+        });
+
         return root;
+    }
+
+    public String post_data(String input){
+        String gen_message = "";
+        if (input!=null) {
+            String query = input.toString();
+            String prompt = query;
+
+            try {
+                URL url = new URL(FLASK_ENDPOINT);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+
+                // Construct the request body
+                String requestBody = "Message=" + prompt;
+
+                OutputStream outputStream = connection.getOutputStream();
+                outputStream.write(requestBody.getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+
+                    // Extract the generated message from the JSON response
+                    gen_message = response.substring(12, (response.length() - 2));
+                } else {
+                    gen_message = "API request failed with response code: " + responseCode;
+                }
+
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+                gen_message = "API request failed: " + e.getMessage();
+
+            }
+        }
+        return gen_message;
     }
 
     @Override
